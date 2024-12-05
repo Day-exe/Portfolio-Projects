3b2ce183e8ee573bcd9710e796ccb48f936f332c
Day <ethanscoggin1999@gmail.com>
2024-12-05 12:24:05 -0500

    Started on the RecipeConverter class and began making its constructor method

--- src/Project 2/RecipeConverter.java
+++ src/Project 2/RecipeConverter.java
@@ -1,5 +1,26 @@
+import java.util.*; //Import utilities
+
 public class RecipeConverter{
 
-    
+    Scanner myObj = new Scanner(System.in);
+
+    private int numIngredients;
+    private String ingredients[];
+    private int quant[];
+    private int quantAlt[];
+    private String measurements[];
+    private String measurementsAlt[];
+
+    public RecipeConverter(int numIngredients, String ingredients[], int quant[], String measurements[]){
+
+        
+
+    }
+
+    public static void main(String[] args){
+
+
+
+    }
 
 }
