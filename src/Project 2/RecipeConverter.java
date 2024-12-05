a49935bd6f8e90a355c5e0c2544590e031b18146
Day <ethanscoggin1999@gmail.com>
2024-12-05 16:33:16 -0500

    small edits and finished constructor. starting on method to convert the recipe into a smaller batch

--- src/Project 2/RecipeConverter.java
+++ src/Project 2/RecipeConverter.java
@@ -2,8 +2,10 @@ import java.util.*; //Import utilities
 
 public class RecipeConverter{
 
+    //scanner for user input
     Scanner myObj = new Scanner(System.in);
 
+    //instance variables
     private int numIngredients;
     private String ingredients[];
     private int quant[];
@@ -11,7 +13,17 @@ public class RecipeConverter{
     private String measurements[];
     private String measurementsAlt[];
 
+    //constructor
     public RecipeConverter(int numIngredients, String ingredients[], int quant[], String measurements[]){
+
+        this.numIngredients = numIngredients;
+        this.ingredients = ingredients;
+        this.quant = quant;
+        this.measurements = measurements;
+
+    }
+
+    public void split(double factor){
 
         
 
